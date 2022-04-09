db810031c0ffa09f6f5ca21a95a2af3197383193
Artem Kravchenko <atyoma.kravchenko@gmail.com>
2022-04-09 23:33:56 +0300

    step 2 done

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -4,16 +4,71 @@
 
 import React from 'react';
 import s from './container.module.css';
+import Controls from './Controls';
 
 class Counter extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    };
+  }
+
+  addingGood = () => {
+    // event
+    // console.log('add Good');
+    // const target = e.target;
+    // console.log(target);
+
+    // this.setState({ good: this.state.good + 1 }); //обновление от регистрации
+
+    this.setState(prevState => {
+      return { good: prevState.good + 1 }; //обновление от текущего значения
+    });
+  };
+
+  addingNeutral = () => {
+    this.setState(prevState => {
+      return { neutral: prevState.neutral + 1 };
+    });
+  };
+  addingBad = () => {
+    this.setState(prevState => {
+      return { bad: prevState.bad + 1 };
+    });
+  };
+
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    if (this.state.good) {
+      return Math.round((100 / this.countTotalFeedback()) * this.state.good);
+    } else return '0';
+  };
+
   render() {
     return (
       <div className={s.container}>
-        <span>0</span>
+        <h1>Please leave feedback</h1>
+        <Controls
+          onGood={this.addingGood}
+          onNeutral={this.addingNeutral}
+          onBad={this.addingBad}
+        />
         <div>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
+          <h2>Statistics</h2>
+          <p>Good:{this.state.good}</p>
+          <p>Neutral:{this.state.neutral}</p>
+          <p>Bad:{this.state.bad}</p>
+          <p>Total:{this.countTotalFeedback()}</p>
+          <p>
+            Positive feedback:
+            {this.countPositiveFeedbackPercentage()}%
+          </p>
         </div>
       </div>
     );

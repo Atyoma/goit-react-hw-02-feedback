d7ba0c0b7dbfb5892400d7d92c982ab314f2daa3
Artem Kravchenko <atyoma.kravchenko@gmail.com>
2022-04-10 14:50:19 +0300

    Done the task

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import App from 'App';
+import { App } from './components/App';
 import ReactDom from 'react-dom';
-// import 'modern-normalize/modern-normalize.css';
 
 ReactDom.render(
   <React.StrictMode>
@@ -9,23 +8,3 @@ ReactDom.render(
   </React.StrictMode>,
   document.querySelector('#root')
 );
-
-// ===========================================================
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { App } from 'components/App';
-// import reportWebVitals from './reportWebVitals';
-// import './index.css';
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
